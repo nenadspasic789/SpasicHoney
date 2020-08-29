520e3abd64896649e2e6dab830e9e51d2f863308
Nenad Spasic <nenadspasic789@gmail.com>
2020-08-29 22:19:47 +0200

    Implement cart menagement mechanism

---
+++ src/controllers/api/user.cart.controller.ts
@@ -0,0 +1,48 @@
+import { CartService } from "src/services/cart/cart.service";
+import { Controller, Get, UseGuards, Req, Body, Post, Patch } from "@nestjs/common";
+import { AllowToRoles } from "src/misc/allow.to.roles.descriptor";
+import { RoleCheckerGuard } from "src/misc/role.checker.guard";
+import { Cart } from "src/entities/cart.entity";
+import { Request } from "express";
+import { AddArticleToCartDto } from "src/dtos/cart/add.article.to.cart.dto";
+import { EditArticleDto } from "src/dtos/article/edit.article.dto";
+import { EditArticleInCartDto } from "src/dtos/cart/edit.article.in.cart.dto";
+
+@Controller('api/user/cart')
+export class UserCartController {
+    constructor (private cartService: CartService) {}
+
+    private async getActiveCartForUserId(userId: number): Promise<Cart> {
+        let cart =  await this.cartService.getLastActiveCartByUserId(userId);
+        if(!cart) {
+            cart =  await this.cartService.createNewCartForUser(userId);
+        }
+        return await this.cartService.getById(cart.cartId);
+    }
+
+    
+    @Get() //  GET http://localhost:3000/api/user/cart/
+    @UseGuards(RoleCheckerGuard)
+    @AllowToRoles('user')
+    async getCurrentCart(@Req() req: Request): Promise<Cart> {
+        return await this.getActiveCartForUserId(req.token.id);
+    }
+
+
+    @Post('addToCart') //  POST http://localhost:3000/api/user/cart/addToCart/
+    @UseGuards(RoleCheckerGuard)
+    @AllowToRoles('user')
+    async addToCart(@Body() data: AddArticleToCartDto, @Req() req: Request): Promise<Cart> {
+        const cart = await this.getActiveCartForUserId(req.token.id);
+        return this.cartService.addArticleToCart(cart.cartId, data.articleId, data.quantity);
+    }
+
+    @Patch() // PATCH http://localhost:3000/api/user/cart/
+    @UseGuards(RoleCheckerGuard)
+    @AllowToRoles('user')
+    async changeQuantity(@Body() data: EditArticleInCartDto, @Req() req: Request): Promise<Cart> {
+        const cart = await this.getActiveCartForUserId(req.token.id);
+        return await this.cartService.changeQuantity(cart.cartId, data.articleId, data.quantity);
+    }
+
+}
